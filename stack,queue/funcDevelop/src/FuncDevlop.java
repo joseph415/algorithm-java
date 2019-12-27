e50db0cea216fe693f0f0a83f53fde79c60cf0ad
Eun Seok <jeseok95@gmail.com>
2019-12-27 14:55:00 +0900

    file 위치변경

---
+++ stack,queue/funcDevelop/src/FuncDevlop.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class FuncDevlop {
+
+    class Solution {
+        public int[] solution(int[] progresses, int[] speeds) {
+            int[] answer = {};
+            return answer;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] p = {93, 30, 55};
+        int[] s = {1, 30, 5};
+        int[] ans = Solution.solution(p,s);
+    }
+}
