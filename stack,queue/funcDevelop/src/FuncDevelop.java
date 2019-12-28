34d0e92f509f88d9fa3963098b0708d9d2a2ac11
Eun Seok <jeseok95@gmail.com>
2019-12-28 16:58:55 +0900

    프로그래머스 기능구현[다른사람풀이]
    개쩐다

--- stack,queue/funcDevelop/src/FuncDevelop.java
+++ stack,queue/funcDevelop/src/FuncDevelop.java
@@ -2,57 +2,18 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int count;
-        List<Integer> answer = new ArrayList<>();
-        Deque<Integer> pro = new ArrayDeque<>();
-        Deque<Integer> temp1 = new ArrayDeque<>();
-        Deque<Integer> temp2 = new ArrayDeque<>();
-        int[] sum = new int[progresses.length];
-        int[] spd = new int[speeds.length];
-
+        int[] dayOfend = new int[100];
+        int day = -1;
         for (int i = 0; i < progresses.length; i++) {
-            int progress = progresses[i];
-            pro.add(progress);
-            temp1.push(progress);
-            temp2.push(speeds[i]);
-        }
-        for(int i=0;i<pro.size();i++){
-            sum[i] = temp1.pop();
-            spd[i] = temp2.pop();
-        }
-
-        while (!pro.isEmpty()) {
-            System.out.println(Arrays.toString(sum));
-            for (int i = 0; i < pro.size(); i++) {
-                sum[i] += spd[i];
+            while (progresses[i] + (day * speeds[i]) < 100) {
+                day++;
             }
-            if (sum[pro.size() - 1] >= 100) {
-                count = 1;
-                if (pro.size() != 1) {
-                    for (int i = pro.size() - 2; i >= 0; i--) {
-                        if (sum[i] >= 100) {
-                            count++;
-                        }
-                        else{
-                            break;
-                        }
-                    }
-                }
-                for (int i = 0; i < count; i++) {
-                    pro.poll();
-                }
-                answer.add(count);
-            }
+            dayOfend[day]++;
         }
-
-        int idx = 0;
-        int[] ans = new int[answer.size()];
-        for (Integer i : answer) {
-            ans[idx++] = i;
-        }
-        return ans;
+        return Arrays.stream(dayOfend).filter(i -> i != 0).toArray();
     }
 }
+
 
 public class FuncDevelop {
     public static void main(String[] args) {
